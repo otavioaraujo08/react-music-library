8928dd4dea3f10acda12245fb9aebe9ca4ee7521
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-11-25 20:35:06 -0300

    feat: setup my route

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { router } from './routes/route';
+import { RouterProvider } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <h1>Olá Mundo</h1>
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
