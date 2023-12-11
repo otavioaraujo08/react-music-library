7f796f9199259e405d8c5b2f06439f91168bb332
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-12-11 11:23:48 -0300

    feat: update path

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { router } from './routes/route';
+import { router } from '@routes/route';
 import { RouterProvider } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
