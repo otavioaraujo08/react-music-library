78079a3c3b9055f01dccf1624fd37d2f98c45558
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-12-18 12:26:04 -0300

    feat: add utils path aliase

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
             '@assets': '/src/assets',
             '@services': '/src/services',
             '@routes': '/src/routes',
+            '@utils': '/src/utils',
         },
     },
 });
