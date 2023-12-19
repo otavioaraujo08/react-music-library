014d40dfa703b7d7657c62dd9f8063ece5e715ce
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-12-19 12:24:20 -0300

    feat: update text and add button

--- src/pages/home/banner/index.tsx
+++ src/pages/home/banner/index.tsx
@@ -1,20 +1,24 @@
 import Carousel from '@components/carousel';
 import { AlbunsObject } from '@utils/albunsObject';
 import { Banner, BannerGrid, CarouselBanner, SubTitle, Title } from './styles';
+import ButtonStyled from '@components/buttonStyled';
 
 export const HomeBanner = () => {
     return (
         <Banner>
             <BannerGrid>
                 <Title>
-                    <strong>Playlists</strong> que possam te ajudar a conquistar
-                    seus <strong>Objetivos!</strong>
+                    <strong>Playlists</strong> que ajudam a conquistar seus{' '}
+                    <strong>Objetivos!</strong>
                 </Title>
+
                 <SubTitle>
                     Precisando daquele gás para alcançar aquela meta ? Nós te
                     ajudamos, com as melhores playlists do Spotify para cada
                     situação!
                 </SubTitle>
+
+                <ButtonStyled title="Redirecionar !" />
             </BannerGrid>
             <CarouselBanner>
                 <Carousel slides={AlbunsObject} />
