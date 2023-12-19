35fc512ed55cc0edd691921cf68eef46574fd896
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-12-19 12:25:21 -0300

    feat: update responsivty and properties styles

--- src/pages/home/banner/styles.ts
+++ src/pages/home/banner/styles.ts
@@ -2,28 +2,46 @@ import styled from 'styled-components';
 
 export const Banner = styled.div`
     width: 95%;
-    height: 55vh;
+    height: 31rem;
     background: #fef8e0;
     border-radius: 4px;
 
     display: grid;
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: repeat(5, 1fr);
     grid-template-rows: 1fr;
     grid-column-gap: 0px;
     grid-row-gap: 0px;
+
+    @media (max-width: 820px) {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 `;
 
 export const BannerGrid = styled.div`
+    grid-area: 1 / 3 / 2 / 6;
+    height: 100%;
+    width: 95%;
     display: flex;
+    flex-wrap: wrap;
     flex-direction: column;
     gap: 1rem;
-    padding: 2rem;
     justify-content: center;
-    align-items: center;
+
+    @media (max-width: 820px) {
+        text-align: center;
+        width: 90%;
+    }
 `;
 
 export const CarouselBanner = styled.div`
+    grid-area: 1 / 1 / 2 / 3;
     overflow: hidden;
+
+    @media (max-width: 820px) {
+        display: none;
+    }
 `;
 
 export const Title = styled.h1`
@@ -38,6 +56,18 @@ export const Title = styled.h1`
         font-weight: 600;
         color: #282b21;
     }
+
+    @media (max-width: 1500px) {
+        font-size: 2.4rem;
+    }
+
+    @media (max-width: 1200px) {
+        font-size: 2rem;
+    }
+
+    @media (max-width: 800px) {
+        font-size: 1.8rem;
+    }
 `;
 
 export const SubTitle = styled.h1`
@@ -47,4 +77,16 @@ export const SubTitle = styled.h1`
     font-family: 'IBM Plex Sans', sans-serif;
     letter-spacing: 0.1rem;
     text-align: left;
+
+    @media (max-width: 1200px) {
+        font-size: 1.4rem;
+    }
+
+    @media (max-width: 800px) {
+        font-size: 1.2rem;
+    }
+
+    @media (max-width: 600px) {
+        font-size: 1rem;
+    }
 `;
