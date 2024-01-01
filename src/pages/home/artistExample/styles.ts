8a2407b092e8050a800407988550859a59cfab7e
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-01-01 20:35:15 -0300

    feat: update stylesheet

--- src/pages/home/artistExample/styles.ts
+++ src/pages/home/artistExample/styles.ts
@@ -1,8 +1,33 @@
+import Meteora from '@assets/images/Meteora.jpg';
 import styled from 'styled-components';
 
 export const Container = styled.div`
+    margin-top: 4rem;
     display: flex;
     height: 100%;
 `;
 
-export const AlbumDiv = styled.div``;
+export const AlbumDiv = styled.div`
+    background-image: url(${Meteora});
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    height: 50vh;
+    min-width: 50vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+`;
+
+export const AlbumsDiv = styled.div`
+    background: #000000;
+    height: 50vh;
+    min-width: 50vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+`;
