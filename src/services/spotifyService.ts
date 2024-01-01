e1af7761fb34048e0cdfb111079cbc079ff3a563
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-01-01 16:48:20 -0300

    feat: remove unused vars and add new function to refresh token

--- src/services/spotifyService.ts
+++ src/services/spotifyService.ts
@@ -1,7 +1,5 @@
 import { api as apiService, ApiService } from './api';
 
-const defaultUrl: string = 'https://api.spotify.com/v1';
-
 interface TokenResponse {
     access_token: string;
     token_type: string;
@@ -37,12 +35,29 @@ class SpotifyService {
         );
     };
 
-    public GetArtist = async () => {
-        return this.api.get(`${defaultUrl}/artists/05fG473iIaoy82BF1aGhL8`, {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('BearerToken')}`,
-            },
-        });
+    public GetRefreshToken = async () => {
+        const client_id = process.env.CLIENT_ID;
+        const refreshToken = localStorage.getItem('BearerToken');
+
+        if (!client_id || !refreshToken) {
+            throw new Error(
+                'As credenciais do cliente não estão configuradas corretamente.'
+            );
+        }
+
+        return this.api.post(
+            'https://accounts.spotify.com/api/token',
+            new URLSearchParams({
+                grant_type: 'refresh_token',
+                refresh_token: refreshToken,
+                client_id: client_id,
+            }),
+            {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            }
+        );
     };
 }
 
