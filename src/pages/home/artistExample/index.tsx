40a88f32a3bbf60dad6a8229ef0818bc34928cce
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-01-02 16:48:45 -0300

    feat: update artist example page component

--- src/pages/home/artistExample/index.tsx
+++ src/pages/home/artistExample/index.tsx
@@ -1,24 +1,91 @@
 import { spotifyArtistService } from '@services/spotifyArtistService';
-import { AlbumDiv, Container } from './styles';
+import {
+    AlbumDiv,
+    AlbumImage,
+    AlbumTitle,
+    AlbumsDiv,
+    BlankDiv,
+    ButtonWithStyle,
+    Container,
+    CoverDiv,
+    NotFoundTitle,
+} from './styles';
+import { useEffect, useState } from 'react';
+import { ArtistAlbumResponse } from 'types/spotifyArtist';
+import ButtonStyled from '@components/buttonStyled';
 
 export const ArtistExample = () => {
+    const [albumsList, setAlbumsList] = useState<ArtistAlbumResponse[]>();
+
     const getArtistAlbums = async () => {
         try {
-            const response = await spotifyArtistService.GetArtistAlbums(
-                '6XyY86QOPPrYVGvF9ch6wz'
-            );
+            if (albumsList) {
+                return;
+            } else {
+                const response = await spotifyArtistService.GetArtistAlbums(
+                    '6XyY86QOPPrYVGvF9ch6wz',
+                    {
+                        limit: 12,
+                    }
+                );
 
-            console.log(response);
+                setAlbumsList(response.items);
+            }
         } catch (err: any) {
             console.log(err);
         }
     };
 
-    return (
+    useEffect(() => {
+        getArtistAlbums();
+    }, []);
+
+    return albumsList?.length ? (
         <Container>
-            <AlbumDiv>
-                <button onClick={getArtistAlbums}>just a button men</button>
-            </AlbumDiv>
+            <CoverDiv>
+                <ButtonWithStyle onClick={getArtistAlbums}>
+                    Acessar o Album
+                </ButtonWithStyle>
+            </CoverDiv>
+
+            <AlbumsDiv>
+                {albumsList.map((album) => {
+                    const { id, name, images, external_urls } = album;
+
+                    const handleRedirectUser = (url: string) => {
+                        return window.open(url, '_blank');
+                    };
+
+                    return (
+                        <AlbumDiv key={id}>
+                            <AlbumImage
+                                src={images[0].url}
+                                alt={name}
+                                onClick={() =>
+                                    handleRedirectUser(external_urls.spotify)
+                                }
+                            />
+
+                            <AlbumTitle
+                                onClick={() =>
+                                    handleRedirectUser(external_urls.spotify)
+                                }
+                            >
+                                {name}
+                            </AlbumTitle>
+                        </AlbumDiv>
+                    );
+                })}
+            </AlbumsDiv>
         </Container>
+    ) : (
+        <BlankDiv>
+            <NotFoundTitle>
+                Sem informações no momento, clique no botão abaixo para
+                recarregar os dados!
+            </NotFoundTitle>
+
+            <ButtonStyled onClick={getArtistAlbums} title="Recarregar" />
+        </BlankDiv>
     );
 };
