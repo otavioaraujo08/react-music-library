2abdf4379a9fac4f264a1c6a0562dbf65100030a
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-01-04 16:45:00 -0300

    fix: update height size problem with no color

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -3,9 +3,10 @@ import styled from 'styled-components';
 export const HomeDiv = styled.div`
     background: #000814;
     color: #ffffff;
-    height: 100vh;
+    height: 100%;
+    min-height: 100vh;
     display: flex;
     flex-direction: column;
     align-items: center;
-    gap: 6rem;
+    gap: 3rem;
 `;
