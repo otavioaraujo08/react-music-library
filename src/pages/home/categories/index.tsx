58c41181977b11f36725ecccc5dbb58ccfd0887a
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-01-04 16:46:16 -0300

    feat: create categories from component

---
+++ src/pages/home/categories/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Title } from './styles';
+
+export const Categories = () => {
+    return (
+        <Container>
+            <Title>Categorias</Title>
+
+            <p>Lista</p>
+        </Container>
+    );
+};
---
+++ src/pages/home/categories/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    height: 100%;
+    min-height: 60vh;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 3rem;
+`;
+
+export const Title = styled.h1`
+    font-size: 2rem;
+    font-weight: 600;
+    font-family: 'IBM Plex Sans', sans-serif;
+    letter-spacing: 0.1rem;
+    margin-left: 3rem;
+`;
