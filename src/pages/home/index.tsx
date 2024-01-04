a2f85149855dab8bc84628d88e5b37c3d61f1d83
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-01-04 16:45:20 -0300

    feat: add categories to home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,6 +4,7 @@ import { HomeBanner } from '@pages/home/banner';
 import { HomeDiv } from './styles';
 import { ArtistExample } from './artistExample';
 import { getToken } from '@utils/getToken';
+import { Categories } from './categories';
 
 export const Home = () => {
     const token = localStorage.getItem('BearerToken');
@@ -19,6 +20,8 @@ export const Home = () => {
             <HomeBanner />
 
             <ArtistExample />
+
+            <Categories />
         </HomeDiv>
     );
 };
